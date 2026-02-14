0d048cb08fdf51b1256fba43d803abc29a59b442
Vlad V <la1worldnub@gmail.com>
2026-02-13 17:19:28 -0800

    feat: Add npm dependencies

---
+++ src/config/constants.js
@@ -0,0 +1,13 @@
+/**
+ * Базовые константы для NEAR Analytics Bot
+ */
+
+const NEARBLOCKS_API_URL = 'https://api.nearblocks.io/v1';
+const API_TIMEOUT = 10000; // 10 секунд
+const MAX_TRANSACTIONS = 50;
+
+module.exports = {
+  NEARBLOCKS_API_URL,
+  API_TIMEOUT,
+  MAX_TRANSACTIONS,
+};
---
+++ src/index.js
@@ -0,0 +1,4 @@
+require('dotenv').config();
+
+// NEAR Analytics Bot - Entry point
+// TODO: Initialize Telegraf bot and command handlers
